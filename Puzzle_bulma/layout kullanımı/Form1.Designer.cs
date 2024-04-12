b863cb09df7de6055f4d024276f9515e5e73f856
GökhanAYDIN <gokhanaydin_243944@hotmail.com>
2024-04-12 20:26:20 +0300

    Add files via upload

---
+++ Puzzle_bulma/layout kullanımı/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace layout_kullanımı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(454, 438);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 19);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(442, 413);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Tag = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 13;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4",
+            "16",
+            "36",
+            "64",
+            "100"});
+            this.comboBox1.Location = new System.Drawing.Point(472, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(533, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Değer.indis";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(533, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Değer";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(604, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(604, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = ":";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(629, 243);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 12;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(629, 268);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 13;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(536, 299);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "kalın";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(536, 322);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(46, 17);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "eğik";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(536, 345);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(58, 17);
+            this.checkBox3.TabIndex = 16;
+            this.checkBox3.Text = "altıçizili";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(533, 129);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "butona basılma sayısı";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(659, 129);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 18;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(533, 178);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(126, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Kaç saniyede çözdünüz.:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(666, 178);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(49, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 464);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Puzzle_bulma/layout kullanımı/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+namespace layout_kullanımı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int koltuksayisi;
+
+
+        int boyut1;
+
+
+        ArrayList deger = new ArrayList();
+        ArrayList indeks = new ArrayList();
+        ArrayList button = new ArrayList();
+        int[] ronaldo;
+        int[,] gametile;
+        int[] gokhan;
+        Random rnd = new Random();
+        static Button[] butonlari_tut;
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel1.Enabled = true;
+            dogrusayisi = 0;
+            int boyut;
+
+
+            zaman = Convert.ToDateTime("00:00:00");
+            saat = zaman.Hour;
+            dakika = zaman.Minute;
+            saniye = zaman.Second;
+
+            timer1.Start();
+
+
+            timer1.Enabled = true;
+            boyut1 = Convert.ToInt32(comboBox1.SelectedItem.ToString());
+
+            boyut = Convert.ToInt32(Math.Sqrt(boyut1));
+
+
+
+            butonlari_tut = new Button[boyut1];
+            gokhan = new int[boyut1]; // sayılar
+            ronaldo = new int[boyut1]; // buton yerleri
+
+
+
+            gametile = new int[boyut, boyut];
+
+
+            List<int> indexarray = new List<int>();
+
+            int upper = boyut * boyut - 1;
+
+            //for (int i = 0; i < boyut1; i++)
+            //{
+            //   butonlari_tut[i]=i; //0..15
+            //    // button.Add(0);
+
+            //}
+
+            for (int i = 0; i < boyut * boyut; i++)
+            {
+                indexarray.Add(i); //0..15
+                // button.Add(0);
+
+            }
+            for (int i = 1; i <= boyut1 / 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    int index = rnd.Next(0, upper--);
+                    gametile[indexarray[index] / (boyut), indexarray[index] % (boyut)] = i;
+                    indexarray.RemoveAt(index);
+
+                }
+
+            }
+
+            int ayse = 0;
+            int tayfun = 0;
+
+            for (int x = 0; x < boyut; x++)
+            {
+                for (int y = 0; y < boyut; y++)
+                {
+                    gokhan[ayse] = gametile[x, y];
+                    ronaldo[ayse] = tayfun;
+                    tayfun++;
+                    ayse++;
+                }
+            }
+
+
+
+            flowLayoutPanel1.Controls.Clear();
+
+            toplam = 0;
+
+            label8.Text = "";
+            label9.Text = "";
+            int say = 0;
+            for (int i = 0; i < boyut1; i++)
+            {
+                Button btn = new Button();
+                btn.Text = Convert.ToString(gokhan[i]);
+                butonlari_tut[say] = new Button();
+                butonlari_tut[say].Text = Convert.ToString(gokhan[i]);
+                say++;
+                int b = rnd.Next(1, boyut / 2 + 1); // 4 lü için eleman üretme
+
+                //  btn.Text=deger[i];
+
+
+
+
+                if (boyut1 == 4)
+                {
+                    butonlari_tut[i].Width = 175;
+                    butonlari_tut[i].Height = 175;
+                    groupBox1.Text = "   2  *  2  ";
+
+                }
+                else  if (boyut1 == 16)
+                {
+                    butonlari_tut[i].Width = 86;
+                    butonlari_tut[i].Height = 86;
+                    groupBox1.Text = "   4  *  4  ";
+                }
+
+            else    if (boyut1 == 36)
+                {
+                    butonlari_tut[i].Width = 60;
+                    butonlari_tut[i].Height = 60;
+                    groupBox1.Text = "   6  *  6  ";
+                }
+
+                else      if (boyut1 == 64)
+                {
+                    butonlari_tut[i].Width = 45;
+                    butonlari_tut[i].Height = 45;
+                    groupBox1.Text = "   8  *  8  ";
+                }
+                else if(boyut1 == 100)
+                    
+                {
+                    butonlari_tut[i].Width = 35;
+                    butonlari_tut[i].Height = 35;
+                    groupBox1.Text = "   10  *  10  ";
+                }
+
+
+
+
+
+
+
+                flowLayoutPanel1.Controls.Add(butonlari_tut[i]);
+                int m = flowLayoutPanel1.Controls.IndexOf(butonlari_tut[i]);
+
+
+                flowLayoutPanel1.AutoScroll = true;
+
+
+                butonlari_tut[i].Click += new EventHandler(btn_Click);
+
+                butonlari_tut[i].BackColor = Color.WhiteSmoke;
+                // renk = btn.BackColor; 
+
+                butonlari_tut[i].Text = "";
+            }
+
+
+            kesisim = new int[2];
+
+        }
+
+        int tus = 0;
+        int[] kesisim;
+        int dogrusayisi = 0;
+        int saat, dakika, saniye;
+        DateTime zaman;
+
+
+        void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            int dede = flowLayoutPanel1.Controls.IndexOf(btn); // basılı butonun indeksi
+            label8.Text = dede.ToString();
+
+            kesisim[tus] = dede;
+
+            label9.Text = gokhan[dede].ToString();
+
+            btn.Text = gokhan[dede].ToString();
+            tus++;
+            label11.Text = tus.ToString();
+
+            if (tus == 2)
+            {
+                if (gokhan[kesisim[tus - 1]] == gokhan[kesisim[tus - 2]]&&kesisim[tus-1]!=kesisim[tus-2])
+                {
+                    butonlari_tut[kesisim[tus - 1]].Enabled = false;
+                    butonlari_tut[kesisim[tus - 2]].Enabled = false;
+
+                    MessageBox.Show("doğru");
+                    dogrusayisi++;
+
+                    if (dogrusayisi == (boyut1 / 2))
+                    {
+                        flowLayoutPanel1.Enabled = false;
+                        timer1.Enabled = false;
+                        zaman = Convert.ToDateTime("00:00:00");
+                    }
+                    tus = 0;
+                }
+                else
+                {
+                    MessageBox.Show("yanlış");
+
+
+
+                    butonlari_tut[kesisim[tus - 1]].Text = "";
+                    butonlari_tut[kesisim[tus - 2]].Text = "";
+                    tus = 0;
+
+
+
+
+                }
+            }
+
+            if (tus > 2)
+                tus = 0;
+
+
+
+        }
+        public int toplam;
+
+
+        public int b;
+        public int c;
+        public int d;
+
+        public Color White { get; set; }
+
+        public Color Pink { get; set; }
+
+        public Color Blue { get; set; }
+
+
+
+
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox1.Checked == true)
+                foreach (Control btn in flowLayoutPanel1.Controls)
+                {
+
+                    btn.Font = new Font(btn.Font.Name,
+                        btn.Font.Size, btn.Font.Style ^ FontStyle.Bold);
+                }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+                foreach (Control btn in flowLayoutPanel1.Controls)
+                {
+
+                    btn.Font = new Font(btn.Font.Name,
+                        btn.Font.Size, btn.Font.Style ^ FontStyle.Italic);
+                }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (checkBox3.Checked == true)
+                foreach (Control btn in flowLayoutPanel1.Controls)
+                {
+
+                    btn.Font = new Font(btn.Font.Name,
+                        btn.Font.Size, btn.Font.Style ^ FontStyle.Underline);
+                }
+
+            else
+                foreach (Control btn in flowLayoutPanel1.Controls)
+                {
+
+                    btn.Font = new Font(btn.Font.Name,
+                        btn.Font.Size, btn.Font.Style ^ FontStyle.Regular);
+                }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label13.Text = ((Convert.ToString(saat) + ":") + (Convert.ToString(dakika) + ":") + Convert.ToString(saniye));
+
+            saniye = saniye + 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
